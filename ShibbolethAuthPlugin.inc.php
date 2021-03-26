c6250ed6649d19b6f7e97eb292af330544e1a715
Arvo Consultores y Tecnología. S.L <arvo@arvo.es>
2021-03-26 13:29:59 +0100

    Update ShibbolethAuthPlugin.inc.php

--- ShibbolethAuthPlugin.inc.php
+++ ShibbolethAuthPlugin.inc.php
@@ -6,7 +6,7 @@
  * Copyright (c) 2014-2017 Simon Fraser University
  * Copyright (c) 2003-2017 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
- * Modified (2021) by Arvo Consultores. Project funded by Fundación Española de Ciencia y Tecnologia (FECYT)  
+ * Code modified (2021) by Josué Cimadevilla at Arvo Consultores. Project funded by Fundación Española de Ciencia y Tecnologia (FECYT)  
  *
  * @class ShibbolethAuthPlugin
  * @ingroup plugins_generic_shibboleth
